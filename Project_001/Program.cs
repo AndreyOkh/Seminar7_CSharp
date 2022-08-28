29602b62dbe6b8923dbafcfd426d805a24c709cf
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-28 10:38:14 +1000

    Задайте двумерный массив размером m×n, заполненный случайными целыми числами.

---
+++ Project_001/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int m = ReadInt("Введите количество строк: ");
+int n = ReadInt("Введите количество столбцов: ");
+
+int[,] matrix = FillMatrix(m, n, 1, 9);
+PrintMatrix(matrix);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+int[,] FillMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
